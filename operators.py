ec0da1e0cc098259078d650e41b5a9068a20879b
srivanirapaka20 <srivanirapaka20@gmail.com>
2025-09-09 18:13:39 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,13 @@
+print(8+2*5)
+print(5*2+3)
+# relation operator
+print(5<8)
+print(5>3)
+print(4==4)
+print(5<=3)
+print(5>=5)
+#bitwise operator
+print(10&20)
+print(10|30)
+print(28^30)
+print(20<<3)
