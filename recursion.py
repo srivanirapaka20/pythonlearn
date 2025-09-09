bc50a3a752b348569a9fce2297a8419b031723ad
srivanirapaka20 <srivanirapaka20@gmail.com>
2025-09-09 18:12:39 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    if n == 0 or n == 1 :
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(5))    
+
+
+def fibonacci(n):
+    if n == 0:
+       return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(6))
